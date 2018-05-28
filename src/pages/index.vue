a7f8241af1d75b679bfabd59a2cc206c38a9a02b
zhubaoda <15971484216@163.com>
2018-05-28 15:49:33 +0800

    侧边栏

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,13 +2,36 @@
   @import url("../assets/style/main.less");
   .index {
   	width: 100%;
-  	
+  	position: relative;
+  	.head-nav {
+  	   .fixed;
+  	   width: 100%;
+  	}
+  	.black {
+  	  .fixed;
+  	  .box(100vw, 100vh);
+  	  background-color: rgba(0, 0, 0, .6);
+  	}
+  	.left-slider {
+  	  .box(80%, 100%);
+  	  background-color: blue;
+  	  position: fixed;
+  	  top: 0;
+  	  left: 0; 
+  	  z-index: 30;
+  	}
   }
 </style>
 
 <template>
   <div class="index">
-    <index-head @switchNav="switchNav" />
+    <index-head @switchNav="switchNav" class="head-nav"/>
+    <div class="black" v-show="sliderShow" @click="cancelSlider"></div>
+    <transition name="slider">
+      <div class="left-slider" v-if="sliderShow">
+    	   23321
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -18,12 +41,19 @@
   	components: { indexHead },
   	data () {
   	  return {
-  	  	
+  	  	sliderShow: false
   	  }
   	},
   	methods: {
+  	  // 头组件事件
   	  switchNav (idx) {
-  	  	console.log(idx)
+  	  	if (idx === 0) {
+  	  	  this.sliderShow = !this.sliderShow;
+  	  	}
+  	  },
+  	  // 让侧边栏消失
+  	  cancelSlider () {
+  	  	this.sliderShow = false;
   	  }
   	}
   }
